c988935808004f3a18bf402a563870fd50f612fa
Victor Gabriel <victorgabriel08fs@gmail.com>
2024-02-16 01:49:16 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useAuth } from "./contexts/AuthContext";
+import Login from "./pages/private/public/Auth/Login";
+import PrivateRoutes from "./routes/index.routes";
+import Loading from "./pages/private/public/Loading";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  const { auth, loading } = useAuth();
+  return loading ? (
+    <Loading/>
+  ) : auth ? (
+    <PrivateRoutes />
+  ) : (
+    <Login />
   );
 }
 
